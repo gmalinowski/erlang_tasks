12c707f2f778a99c53be6bcd67c7909ca768f1cd
Grzegorz Malinowski <git.15m27@passmail.net>
2026-01-23 15:00:21 +0100

    Refactor strain.

--- strain/src/strain.erl
+++ strain/src/strain.erl
@@ -11,11 +11,5 @@ keep(Fn, [Item | Tail], Result) ->
     _ -> keep(Fn, Tail, Result)
   end.
 
-discard(Fn, List) -> discard(Fn, List, []).
+discard(Fn, List) -> keep(fun(Item) -> not Fn(Item) end, List).
 
-discard(_Fn, [], Result) -> lists:reverse(Result);
-discard(Fn, [Item | Tail], Result) ->
-  case Fn(Item) of
-    true -> discard(Fn, Tail, Result);
-    _ -> discard(Fn, Tail, [Item | Result])
-  end.
