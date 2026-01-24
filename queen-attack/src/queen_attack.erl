5b6a57ac56e002eb4f4b380366c2dba5f1e08d6d
Grzegorz Malinowski <git.15m27@passmail.net>
2026-01-24 19:03:16 +0100

    refactor code

--- queen-attack/src/queen_attack.erl
+++ queen-attack/src/queen_attack.erl
@@ -3,7 +3,8 @@
 -export([can_attack/2]).
 
 
-can_attack({X1, Y1}, {X2, Y2}) when X1 =:= X2 orelse Y1 =:= Y2 -> true;
+can_attack({X, _}, {X, _}) -> true;
+can_attack({_, Y}, {_, Y}) -> true;
 can_attack({X1, Y1}, {X2, Y2}) -> abs(X1 - X2) =:= abs(Y1 - Y2).
 
 
